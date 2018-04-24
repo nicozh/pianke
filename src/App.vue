4a9d8813be04db657613b722e341e9768e4ef9fc
zhang <mitmiky@outlook.com>
2018-04-24 23:42:22 +0800

    初始化

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="app">
+    <header>Header</header>
+    <main>Main</main>
+    <footer>Footer</footer>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "App"
+};
+</script>
+
+<style>
+header,
+footer {
+  height:90px;
+  background-color: #b3c0d1;
+  color: #333;
+  text-align: center;
+  line-height: 60px;
+}
+
+main {
+  background-color: #e9eef3;
+  color: #333;
+  text-align: center;
+  line-height: 160px;
+}
+
+</style>
