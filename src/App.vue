c449adadbedb1dfe4bbde6383817b3c9d5be5d83
zhang <mitmiky@outlook.com>
2018-04-25 23:56:43 +0800

    布局header/添加焦点图片组件

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,119 @@
 <template>
   <div id="app">
-    <header>Header</header>
-    <main>Main</main>
+    <transition name="fade">
+      <header v-show="headShow">
+          <div class="head">
+            <div class="head-logo"></div>
+            <nav>
+            <ul>
+              <li><a href="">首页</a></li>
+              <li><a href="">阅读</a></li>
+              <li><a href="">电台</a></li>
+              <li><a href="">碎片</a></li>
+              <li><a href="">客户端</a></li>
+            </ul>
+              <div class="nav-icon">
+                <div class="write">书写</div>
+                <div class="notice">通知</div>
+                <div class="user-logo">用户</div>
+              </div>
+            </nav>
+          </div>
+      </header>
+    </transition>
+    <main>
+      <focus></focus>
+    </main>
     <footer>Footer</footer>
   </div>
 </template>
 
 <script>
+import focus from "./components/home/focus";
+
 export default {
-  name: "App"
+  name: "App",
+  data() {
+    return {
+      headShow: true
+    };
+  },
+  components: { focus },
+  methods: {
+    menu() {
+      scroll = document.documentElement.scrollTop || document.body.scrollTop;
+      scroll > 7 ? this.headShow = false : this.headShow = true
+    }
+  },
+  mounted() {
+    window.addEventListener("scroll", this.menu);
+  },
+  computed: {}
 };
 </script>
 
 <style>
+@import url(./reset.css);
+
+.fade-enter-active,
+.fade-leave-active {
+  transition: margin-top 0.5s;
+}
+.fade-enter,
+.fade-leave-to {
+  margin-top: -90px;
+}
+
 header,
 footer {
-  height:90px;
-  background-color: #b3c0d1;
+  height: 90px;
+  background-color: #ffffff;
   color: #333;
   text-align: center;
-  line-height: 60px;
 }
-
+header {
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  box-shadow: 0 1px 4px 0 #ececec;
+}
+.head {
+  margin: 0 73px;
+  height: 100%;
+  display: flex;
+}
+.head-logo {
+  background-image: url(./assets/head-logo.png);
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: contain;
+  background-color: #fff;
+  width: 80px;
+  height: 90px;
+}
 main {
-  background-color: #e9eef3;
   color: #333;
-  text-align: center;
-  line-height: 160px;
+  padding-top: 90px;
+  margin: 20px 73px 0;
 }
 
+nav {
+  display: flex;
+  justify-content: space-between;
+  height: 100%;
+  align-items: center;
+  margin-left: 172px;
+  flex: 1;
+}
+.nav-icon {
+  display: flex;
+}
+
+ul {
+  display: flex;
+}
+li {
+  padding: 26px;
+}
 </style>
---
+++ src/components/home/focus.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="focus">
+      <div class="left-p">
+      
+      </div>
+      <div class="right-p">
+        <ul>
+          <li><a href=""><img src="" alt=""></a></li>
+          <li><a href=""><img src="" alt=""></a></li>
+          <li><a href=""><img src="" alt=""></a></li>
+          <li><a href=""><img src="" alt=""></a></li>
+        </ul>
+      </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "focus",
+  data() {
+    return {};
+  }
+};
+</script>
+
+<style scoped>
+.focus {
+  margin: 0 120px;
+  border: 1px solid red;
+  display: flex;
+}
+.left-p{
+  width: 480px;
+  height: 480px;
+  border: 1px solid red;
+}
+.right-p>ul{
+  display: flex;
+  flex-wrap: wrap
+}
+ul>li{
+  width: 240px;
+  height: 240px;
+  border: 1px solid blue;
+}
+</style>
